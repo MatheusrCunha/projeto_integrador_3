7c3be35fe5261fca8437944950f97130c2385b98
uesleimarian <uesleimarian@gmail.com>
2025-01-24 00:15:47 -0300

    Funcões RTC e alarme
    Criado arquivo rtc.c
    Criado arquivo rtc.h

--- Firmware/feeder/main/main.c
+++ Firmware/feeder/main/main.c
@@ -7,6 +7,7 @@
 #include "servo.h"
 #include "driver_init.h"
 #include "defines.h"
+#include "rtc.h"
 
 //Funções apenas para testes de aplicação
 uint32_t lastMillis = 0;
@@ -14,6 +15,8 @@ bool bttouch = false;
 void test_touch_buttons(void);
 void test_delay_tick(void);
 void test_move_servo(void);
+void test_servo_and_rtc(void);
+void test_alarme(void);
 
 
 
@@ -36,6 +39,9 @@ void app_main(void) {
     init_servo(); // Inicializa o timer e pinos do servo
 
 
+    //test_servo_and_rtc();
+    test_alarme();
+
     while (1) {
         //Executa função de teste
         test_touch_buttons();
@@ -45,6 +51,57 @@ void app_main(void) {
     }
 }
 
+void test_alarme(void){
+
+    init_rtc();
+    print_date();
+
+    for (size_t i = 0; i < 25; i++)
+    {
+        printf("Retorno de inserção alarme: %s\n", insert_alarm(get_time() + (60*i)));
+    }
+    
+    time_t alarm_list[MAX_ALARMS];
+    int alarm_count = get_all_alarms(alarm_list);
+
+
+    if (alarm_count == 0) {
+        //TODO  //Responde para o aplicativo "Nenhum alarme programado.\n"
+        printf("Nenhum alarme programado.\n");
+    } else {
+        //TODO // //Responde para o aplicativo "Lista de alarmes programados:\n"
+        printf("Lista de alarmes programados:\n");
+        for (int i = 0; i < alarm_count; i++) {
+            struct tm *alarm_time = localtime(&alarm_list[i]);
+            printf("Alarme %d: %02d:%02d\n", 
+                   i + 1, 
+                   alarm_time->tm_hour, 
+                   alarm_time->tm_min);
+        }
+    }
+    
+
+    while (1) {
+        print_date();
+        check_alarm();
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+
+}
+
+
+void test_servo_and_rtc(void){
+
+    init_rtc();
+
+    while (1) {
+        print_date();
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+
+}
 
 void test_touch_buttons(void) {
     sweep_buttons();
